e7f40549e90c7cc06cbd3c6c22bf8e9ed96860c4
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-07-24 17:51:21 -0300

    Checking in all files

---
+++ src/mn/david/cheapertickets/CheaperTickets.groovy
@@ -0,0 +1,63 @@
+package mn.david.cheapertickets
+
+/**
+ * User: David
+ * Date: 16/07/11
+ * Time: 20:22
+ */
+//@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1' )
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.ContentType
+import mn.david.cheapertickets.request.SearchRequest
+import mn.david.cheapertickets.request.StatusRequest
+import groovyx.net.http.Method
+import groovyx.net.http.HttpResponseDecorator
+import javax.servlet.http.Cookie
+import org.apache.http.impl.cookie.BasicClientCookie
+
+def http = new HTTPBuilder('http://www.submarinoviagens.com.br/Passagens/UIService/Service.svc/')
+
+def request = new SearchRequest();
+request.origin.with {
+    code = 'POA'
+    departureDay = Date.parse("dd/MM/yyyy", "23/09/2011");
+}
+request.destination.with {
+    code = 'BHZ'
+    departureDay = Date.parse("dd/MM/yyyy", "25/09/2011");
+}
+def  out = new File('C:\\Temp\\out.txt').newWriter();
+http.post(path: 'SearchGroupedFlights', body: request.toString(), requestContentType: ContentType.JSON) { resp, json ->
+    out << "SearchGroupedFlights - ${json}"
+    out.newLine();
+    if (!json.Errors && json.PullStatusFrom != null) {
+        def status = json.Status;
+        def pullStatusFrom = new URL(json.PullStatusFrom);
+        def searchId = json.SearchId;
+        while (status == 0) {
+            Thread.sleep(5000);
+            def statusRequest = new StatusRequest(pullStatusFrom: pullStatusFrom, searchId: searchId);
+            http.request(Method.POST, ContentType.JSON) {  req ->
+                uri.path = 'GetSearchStatus';
+                body = statusRequest.toString();
+                response.failure = { HttpResponseDecorator rs ->
+                    status = 1;
+                    System.out << rs.entity.content;
+                }
+
+                response.success = {statusResp, statusJson ->
+                    status = statusJson.Status;
+                    pullStatusFrom = new URL(statusJson.PullStatusFrom);
+                    searchId = statusJson.SearchId;
+                    if(statusJson.Errors){
+                        status = 1;
+                        println statusJson.Errors
+                    }
+                    out << "GetSearchStatus - ${statusJson}"
+                    out.newLine();
+                }
+            }
+        }
+    }
+}
+out.close();
