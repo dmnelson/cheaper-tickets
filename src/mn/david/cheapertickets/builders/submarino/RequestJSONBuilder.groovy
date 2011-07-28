6a775d5768616e311898b09728bd383903127683
David Nelson <davidmichael.nelson@gmail.com>
2011-07-28 12:41:17 -0300

    Removing 'out.txt': Not supposed to be commited

---
+++ src/mn/david/cheapertickets/builders/submarino/RequestJSONBuilder.groovy
@@ -0,0 +1,11 @@
+package mn.david.cheapertickets.builders.submarino
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dnelson
+ * Date: 7/28/11
+ * Time: 12:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class RequestJSONBuilder {
+}
