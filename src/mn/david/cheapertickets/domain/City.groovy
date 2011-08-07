06e9d97563eb0a79f95460a4dc634ed668939d44
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-08-07 19:48:09 -0300

    Solving conflicts

--- src/mn/david/cheapertickets/domain/City.groovy
+++ src/mn/david/cheapertickets/domain/City.groovy
@@ -16,23 +16,9 @@ enum City {
         this.name = name;
     }
 
-<<<<<<< HEAD
-    static Destination get(String value) {
-        def valueByCode = Destination.valueOf value;
-        return valueByCode ?: Destination.values().find { it.name.equalsIgnoreCase(value) }
-=======
-    static City getDestination(String value) {
-        City valueOfEnum = Enum.valueOf(City, value);
-        if(!valueOfEnum){
-            for (it in City.values()) {
-                if (it.name.equalsIgnoreCase(value)) {
-                    return it;
-                }
-            }
-            return null;
-        }
-        return valueOfEnum;
->>>>>>> 25d41cb38531f7e1a4c26d2cb7c2f8a0b5d9e85e
+    static City get(String value) {
+        def valueByCode = valueOf value;
+        return valueByCode ?: values().find { it.name.equalsIgnoreCase(value) }
     }
 
 }
