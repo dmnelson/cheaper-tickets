3398d2f3fe5ddd2976aebf0ccb91f6fc13f6a644
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-08-07 15:59:30 -0300

    Organizing application domain

--- src/mn/david/cheapertickets/domain/City.groovy
+++ src/mn/david/cheapertickets/domain/City.groovy
@@ -1,25 +1,25 @@
-package mn.david.cheapertickets
+package mn.david.cheapertickets.domain
 
 /**
  * User: David Nelson <http://github.com/dmnelson>
  * Date: 7/31/11
  * Time: 10:48 PM
  */
-enum Destination {
+enum City {
 
     POA('Porto Alegre'),
     BHZ('Belo Horizonte')
 
     final String name;
 
-    private Destination(String name) {
+    private City(String name) {
         this.name = name;
     }
 
-    static Destination getDestination(String value) {
-        Destination valueOfEnum = Enum.valueOf(Destination, value);
+    static City getDestination(String value) {
+        City valueOfEnum = Enum.valueOf(City, value);
         if(!valueOfEnum){
-            for (it in Destination.values()) {
+            for (it in City.values()) {
                 if (it.name.equalsIgnoreCase(value)) {
                     return it;
                 }
