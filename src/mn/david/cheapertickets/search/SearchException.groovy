f2a123770bb2f2aee65601b6b2d23d2ed8085029
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-08-09 01:16:16 -0300

    Using Groovy AST transformation for automatic constructors inheritance

--- src/mn/david/cheapertickets/search/SearchException.groovy
+++ src/mn/david/cheapertickets/search/SearchException.groovy
@@ -1,23 +1,12 @@
 package mn.david.cheapertickets.search
 
+import groovy.transform.InheritConstructors
+
 /**
  * User: David Nelson <http://github.com/dmnelson>
  * Date: 8/5/11
  * Time: 9:35 PM
  */
+@InheritConstructors
 class SearchException extends RuntimeException {
-
-    public SearchException(Throwable throwable) {
-        super(throwable);
-    }
-
-    public SearchException(String s, Throwable throwable) {
-        super(s, throwable);
-    }
-
-    public SearchException(String s) {
-        super(s);
-    }
-
-    public SearchException() {}
 }
