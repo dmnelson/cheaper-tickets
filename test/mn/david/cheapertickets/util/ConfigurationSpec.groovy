bf6e6bb007d46b33f2b85d3595abc0652a65bff3
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-08-09 01:15:26 -0300

    Adding some log to make debug easier

--- test/mn/david/cheapertickets/util/ConfigurationSpec.groovy
+++ test/mn/david/cheapertickets/util/ConfigurationSpec.groovy
@@ -2,18 +2,20 @@ package mn.david.cheapertickets.util
 
 import spock.lang.Specification
 import groovy.mock.interceptor.MockFor
+import groovy.util.logging.Commons
 
 /**
  * User: David Nelson <http://github.com/dmnelson>
  * Date: 8/6/11
  * Time: 10:45 PM
  */
+@Commons
 class ConfigurationSpec extends Specification {
 
-    def "retrieving a configuration"() {
+    def "Retrieving a configuration"() {
 
         given:  "A Configuration class that returns an ordinary mock"
-            println 'setup'
+            log.info 'Test: Retrieving a configuration'
             def mock = mockForConfigObject();
             Configuration.metaClass.'static'.getConfig = {
                 return mock.proxyInstance();
@@ -35,8 +37,10 @@ class ConfigurationSpec extends Specification {
     }
 
 
-    def "loading all configuration from a file"(){
+    def "Loading all configuration from a file"(){
+
         given: "A temp file"
+            log.info 'Test: Loading all configuration from a file'
             File configTempFile = createTempConfigFile("""
                                         my {
                                             test {
@@ -44,22 +48,23 @@ class ConfigurationSpec extends Specification {
                                             }
                                         }
                                     """);
-            Configuration.metaClass.'static'.getConfigFile = {
+            Configuration.metaClass.'static'.getConfigScript = {
                 configTempFile.toURL();
             }
 
-        and: "A mocked 'getConfigFile' method that returns that file"
+        and: "A mocked 'getConfigScript' method that returns that file"
 
 
         when: "The deepest configuration is acessed"
             def deepestConfig = Configuration.config.my.test.config;
-            println Configuration.config;
+            log.info "Deepest config: ${deepestConfig}"
 
         then: "The value of the configuration should be the same as defined on the file"
             deepestConfig == 'WooHoo';
 
         when: "One of the intermediate level configuration is acessed"
             def anotherConfig = Configuration.config.my.test
+            log.info "Intermediate config: ${anotherConfig}"
 
         then: "A wrapper object should be returned"
             anotherConfig instanceof ConfigObject
@@ -70,9 +75,11 @@ class ConfigurationSpec extends Specification {
             Configuration.config = null;
     }
 
-    def "actual config file" (){
+    def "Loading from actual config file" (){
+
         given:
-            def configFile = Configuration.getConfigFile();
+            log.info 'Test: Loading from actual config file'
+            def configFile = Configuration.getConfigScript();
             def config = Configuration.config;
             def configMap = config?.flatten();
 
