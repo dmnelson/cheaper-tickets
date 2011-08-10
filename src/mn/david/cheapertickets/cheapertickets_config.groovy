7ef8b5b74281e234ef60f1c29739054dd76e94ab
David Nelson <davidmichael.nelson@gmail.com>
2011-08-10 13:26:28 -0300

    Additional configuration

--- src/mn/david/cheapertickets/cheapertickets_config.groovy
+++ src/mn/david/cheapertickets/cheapertickets_config.groovy
@@ -3,9 +3,11 @@ package mn.david.cheapertickets
 cheaperTickets {
 
     dateFormat = 'dd/MM/yyyy'
+    defaultEngine = 'submarino'
 
     engine {
         submarino {
+            engineClass = mn.david.cheapertickets.search.engine.submarino.SubmarinoEngine
             webservice {
                 baseURL = "http://www.submarinoviagens.com.br/Passagens/UIService/Service.svc/"
                 search = "SearchGroupedFlights"
