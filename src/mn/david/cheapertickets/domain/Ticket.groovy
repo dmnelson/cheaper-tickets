6a812b0542177b9df21925b1503922907bdb32c1
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-08-17 01:33:18 -0300

    Added more information on domain (company name and arrival date time)

--- src/mn/david/cheapertickets/domain/Ticket.groovy
+++ src/mn/david/cheapertickets/domain/Ticket.groovy
@@ -13,15 +13,44 @@ class Ticket implements Comparable<Ticket> {
     City destination;
     BigDecimal cost;
     Date departure;
+    Date arrival;
+    String company;
 
     int compareTo(Ticket o) {
         cost?.compareTo(o?.cost);
     }
 
+    boolean equals(o) {
+        if (this.is(o)) return true;
+        if (!(o instanceof Ticket)) return false;
+
+        Ticket ticket = (Ticket) o;
+
+        if (arrival != ticket.arrival) return false;
+        if (company != ticket.company) return false;
+        if (cost != ticket.cost) return false;
+        if (departure != ticket.departure) return false;
+        if (destination != ticket.destination) return false;
+        if (origin != ticket.origin) return false;
+
+        return true;
+    }
+
+    int hashCode() {
+        int result;
+        result = (origin != null ? origin.hashCode() : 0);
+        result = 31 * result + (destination != null ? destination.hashCode() : 0);
+        result = 31 * result + (cost != null ? cost.hashCode() : 0);
+        result = 31 * result + (departure != null ? departure.hashCode() : 0);
+        result = 31 * result + (arrival != null ? arrival.hashCode() : 0);
+        result = 31 * result + (company != null ? company.hashCode() : 0);
+        return result;
+    }
+
     @Override
     String toString() {
-        DateFormat format = DateFormat.getDateInstance(DateFormat.FULL, new Locale("pt", "BR"))
-        "${origin?.name} -> ${destination?.name} | ${format.format(departure)} - R\$ ${cost}"
+        DateFormat format = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, new Locale("pt", "BR"))
+        "${origin?.name} -> ${destination?.name} | ${departure ? format.format(departure) : '--'} - R\$ ${cost}"
     }
 
 
