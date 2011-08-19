9afbae81f77a29d26909a5aa8c85418ec2bc008f
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-08-19 00:04:55 -0300

    Replacing equals and hashCode methods for Groovy AST Transformations

--- src/mn/david/cheapertickets/domain/Ticket.groovy
+++ src/mn/david/cheapertickets/domain/Ticket.groovy
@@ -1,12 +1,15 @@
 package mn.david.cheapertickets.domain
 
 import java.text.DateFormat
+import groovy.transform.EqualsAndHashCode
 
 /**
  * User: David
  * Date: 07/08/11
  * Time: 15:48
  */
+
+@EqualsAndHashCode
 class Ticket implements Comparable<Ticket> {
 
     City origin;
@@ -25,32 +28,6 @@ class Ticket implements Comparable<Ticket> {
                             company <=> o.company;
     }
 
-    boolean equals(o) {
-        if (this.is(o)) return true;
-        if (!(o instanceof Ticket)) return false;
-
-        Ticket ticket = (Ticket) o;
-
-        if (arrival != ticket.arrival) return false;
-        if (company != ticket.company) return false;
-        if (cost != ticket.cost) return false;
-        if (departure != ticket.departure) return false;
-        if (destination != ticket.destination) return false;
-        if (origin != ticket.origin) return false;
-
-        return true;
-    }
-
-    int hashCode() {
-        int result;
-        result = (origin != null ? origin.hashCode() : 0);
-        result = 31 * result + (destination != null ? destination.hashCode() : 0);
-        result = 31 * result + (cost != null ? cost.hashCode() : 0);
-        result = 31 * result + (departure != null ? departure.hashCode() : 0);
-        result = 31 * result + (arrival != null ? arrival.hashCode() : 0);
-        result = 31 * result + (company != null ? company.hashCode() : 0);
-        return result;
-    }
 
     @Override
     String toString() {
