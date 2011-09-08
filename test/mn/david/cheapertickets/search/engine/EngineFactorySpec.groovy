b01a11f62a1896e9a6f34508f23e74b14132fafe
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-07 23:51:11 -0300

    Test for EngineFactory

--- test/mn/david/cheapertickets/search/engine/EngineFactorySpec.groovy
+++ test/mn/david/cheapertickets/search/engine/EngineFactorySpec.groovy
@@ -1,9 +1,15 @@
 package mn.david.cheapertickets.search.engine
 
+import spock.lang.Specification
+
 /**
  * User: David Nelson <http://github.com/dmnelson>
  * Date: 07/09/11
  * Time: 23:20
  */
-class EngineFactorySpec {
+class EngineFactorySpec extends Specification {
+
+    def "some test"() {
+
+    }
 }
