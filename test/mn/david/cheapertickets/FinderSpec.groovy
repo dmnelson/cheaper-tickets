6ad935422a97f218f4acfc6569227e6794214eb1
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-07 22:55:46 -0300

    Test for Finder
    Create the test cases for Finder class

--- test/mn/david/cheapertickets/FinderSpec.groovy
+++ test/mn/david/cheapertickets/FinderSpec.groovy
@@ -3,6 +3,7 @@ package mn.david.cheapertickets
 import mn.david.cheapertickets.domain.City
 import mn.david.cheapertickets.search.SearchQuery
 import spock.lang.Specification
+import mn.david.cheapertickets.search.engine.Engine
 
 /**
  * User: David Nelson <http://github.com/dmnelson>
@@ -11,40 +12,20 @@ import spock.lang.Specification
  */
 class FinderSpec extends Specification {
 
+
+
     def "Dummy"() {
-        setup:
 
-        def allTickets = new TreeSet();
-        ['02/09/2011', '09/09/2011', '16/09/2011', '23/09/2011', '30/09/2011'].each { date ->
-            def tickets = new Finder().airfares {
-                from BHZ to POA at date
+        given:
+            Engine engine = Mock(Engine);
+            Finder finder = new Finder(engine);
+
+        when:
+            finder.airfares {
+                from 'Belo Horizonte' to 'Porto Alegre' at '12/06/2011'
             }
-            allTickets.addAll(tickets)
-            tickets = new Finder().airfares {
-                from POA to BHZ at date
-            }
-            allTickets.addAll(tickets)
-        }
 
-         ['04/09/2011', '11/09/2011', '18/09/2011', '25/09/2011', '02/10/2011'].each { date ->
-           def tickets = new Finder().airfares {
-                from BHZ to POA at date
-            }
-            allTickets.addAll(tickets)
-            tickets = new Finder().airfares {
-                from POA to BHZ at date
-            }
-            allTickets.addAll(tickets)
-        }
-
-        println "-- Tickets"
-        allTickets?.each {
-            println it;
-        }
-
-
-
-        expect:
-        1 == 1
+        then:
+            1 * engine.search(new SearchQuery(origin: City.BHZ, destination: City.POA, departureDate: Date.parse('dd/MM/yyyy','12/06/2011')));
     }
 }
