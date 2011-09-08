9cd80f52a4453c4b2ec9399f492bdff3afc9d116
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-07 22:44:47 -0300

    Adding setup/cleanup methods on ConfigurationSpec

--- test/mn/david/cheapertickets/configuration/ConfigurationSpec.groovy
+++ test/mn/david/cheapertickets/configuration/ConfigurationSpec.groovy
@@ -14,28 +14,27 @@ class ConfigurationSpec extends Specification {
 
     def "Retrieving a configuration"() {
 
-        given:  "A Configuration class that returns an ordinary mock"
-            def mock = mockForConfigObject();
-            def configurationInstance = new Configuration();
-            configurationInstance.config = mock.proxyInstance();
+        given: "A Configuration class that returns an ordinary mock"
+        def mock = mockForConfigObject();
+        def configurationInstance = new Configuration();
+        configurationInstance.config = mock.proxyInstance();
 
         when: "A configuration entry is acessed"
-            def configuration = configurationInstance.config;
-            def aConfig = configuration.cheaperTickets.a.config;
+        def configuration = configurationInstance.config;
+        def aConfig = configuration.cheaperTickets.a.config;
 
         then: "The value should be a ConfigObject and not null"
-            aConfig != null;
-            aConfig instanceof ConfigObject;
-            mock.verify(configuration);
+        aConfig != null;
+        aConfig instanceof ConfigObject;
+        mock.verify(configuration);
 
     }
 
 
-    def "Loading all configuration from a file"(){
+    def "Loading all configuration from a file"() {
 
         given: "A temp file"
-            Configuration.configurationInstance = null;
-            File configTempFile = writeConfigFile("""
+        File configTempFile = writeConfigFile("""
                                         my {
                                             test {
                                                 config = 'WooHoo'
@@ -44,63 +43,63 @@ class ConfigurationSpec extends Specification {
                                     """);
 
         and: "A mocked 'getConfigScript' method that returns that file"
-            Configuration configuration = new Configuration();
-            configuration.build(configTempFile.toURL())
+        Configuration configuration = new Configuration();
+        configuration.build(configTempFile.toURL())
 
         when: "The deepest configuration is acessed"
-            def deepestConfig = configuration.config.my.test.config;
+        def deepestConfig = configuration.config.my.test.config;
 
         then: "The value of the configuration should be the same as defined on the file"
-            deepestConfig == 'WooHoo';
+        deepestConfig == 'WooHoo';
 
         when: "One of the intermediate level configuration is acessed"
-            def anotherConfig = configuration.config.my.test
+        def anotherConfig = configuration.config.my.test
 
         then: "A wrapper object should be returned"
-            anotherConfig instanceof ConfigObject
+        anotherConfig instanceof ConfigObject
 
         when: "Using the configuration on the standard (static) way"
-            def config = Configuration.get { it };
-            def deepestFromDefault = Configuration.get{ my.test.config }
+        def config = Configuration.get { it };
+        def deepestFromDefault = Configuration.get { my.test.config }
 
         then: "It should be the same as the manual way"
-            config == configuration.config;
-            deepestConfig == deepestFromDefault;
+        config == configuration.config;
+        deepestConfig == deepestFromDefault;
 
         cleanup: "Deleting the file and reseting the Configuration metaclass"
-            assert configTempFile.delete();
-            Configuration.configurationInstance = null;
+        assert configTempFile.delete();
+
     }
 
-    def "Loading from actual config file" (){
+    def "Loading from actual config file"() {
 
         given:
-            def configuration = new Configuration();
-            def configFile = configuration.loadDefaultConfigScript();
+        def configuration = new Configuration();
+        def configFile = configuration.loadDefaultConfigScript();
 
         expect:
-            configFile != null;
+        configFile != null;
 
         when:
-            configuration.build(configFile)
-            def config = configuration.config;
-            def configMap = config?.flatten();
+        configuration.build(configFile)
+        def config = configuration.config;
+        def configMap = config?.flatten();
 
         then:
-            config != null;
-            config instanceof ConfigObject;
-            !configMap.isEmpty()
+        config != null;
+        config instanceof ConfigObject;
+        !configMap.isEmpty()
 
         when:
-            configuration.build();
+        configuration.build();
 
         then:
-            config == configuration.config;
+        config == configuration.config;
 
     }
 
     private static MockFor mockForConfigObject() {
-        new MockFor(ConfigObject,true).with { it ->
+        new MockFor(ConfigObject, true).with { it ->
             demand.containsKey(0..1) { key -> true }
             demand.get(0..1) { name -> it.proxyInstance(); }
             return it;
@@ -114,5 +113,13 @@ class ConfigurationSpec extends Specification {
             w << contents
         }
         return configFile;
+    }
+
+    def setup() {
+        Configuration.configurationInstance = null;
+    }
+
+    def cleanup() {
+        Configuration.configurationInstance = null;
     }
 }
