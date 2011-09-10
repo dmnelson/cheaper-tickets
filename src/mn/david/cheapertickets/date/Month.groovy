a9008ab536008c4e198cad45b01d08935fba9be4
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-10 20:13:34 -0300

    Creating month behaviour and tests
    Creation of some methods on the month class for
    numeric operation with months (e.g. March + 1 =  April)
    and the proper tests for each one of them.

--- src/mn/david/cheapertickets/date/Month.groovy
+++ src/mn/david/cheapertickets/date/Month.groovy
@@ -7,8 +7,32 @@ package mn.david.cheapertickets.date
  */
 enum Month {
 
-    JANUARY, FEBRUARY, MARCH, APRIL,
-    MAY, JUNE, JULY, AUGUST, SEPTEMBER,
-    OCTOBER, NOVEMBER, DECEMBER
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE,
+    JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+
+
+    Month plus(int i) {
+        int sum = ordinal() + i;
+        int numOfMonths = values().size();
+        return Month[sum % numOfMonths];
+    }
+
+    Month minus(int i) {
+        plus(i * -1);
+    }
+
+    Month next() {
+        return this + 1;
+    }
+
+    static Month getAt(int i) {
+        return values()[i];
+    }
+
+    static Month getCurrentMonth() {
+        return Month[Calendar.getInstance().get(Calendar.MONTH)]
+    }
+
+
 
 }
