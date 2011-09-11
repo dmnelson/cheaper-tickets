44e1da60ff0626b68d0873f13b05a084acde963f
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-10 22:21:57 -0300

    Change example code

--- src/mn/david/cheapertickets/CheaperTickets.groovy
+++ src/mn/david/cheapertickets/CheaperTickets.groovy
@@ -8,22 +8,7 @@ package mn.david.cheapertickets
 
 def allTickets = new TreeSet();
 
-['02/09/2011', '09/09/2011', '16/09/2011', '23/09/2011', '30/09/2011'].each { date ->
-    def tickets = new Finder().airfares {
-        from BHZ to POA at date
-    }
-    allTickets.addAll(tickets)
-    tickets = new Finder().airfares {
-        from POA to BHZ at date
-    }
-    allTickets.addAll(tickets)
-}
-
-['04/09/2011', '11/09/2011', '18/09/2011', '25/09/2011', '02/10/2011'].each { date ->
-    def tickets = new Finder().airfares {
-        from BHZ to POA at date
-    }
-    allTickets.addAll(tickets)
+['16/09/2011'].each { date ->
     tickets = new Finder().airfares {
         from POA to BHZ at date
     }
@@ -31,6 +16,6 @@ def allTickets = new TreeSet();
 }
 
 println "-- Tickets"
-allTickets?.each {
+allTickets?.findAll{ it.departure.hours > 12 }.each {
     println it;
 }
