53e2fab61f4c450b00de49018b85e7b3b3dab248
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-11 23:17:38 -0300

    New exception for invalid search queries
    Created a new exception to be thrown when invalid
    search queries are passed as parameter to searcher classes

---
+++ src/mn/david/cheapertickets/search/InvalidQueryException.groovy
@@ -0,0 +1,12 @@
+package mn.david.cheapertickets.search
+
+import groovy.transform.InheritConstructors
+
+/**
+ * User: David Nelson <http://github.com/dmnelson>
+ * Date: 11/09/11
+ * Time: 02:23
+ */
+@InheritConstructors
+class InvalidQueryException extends RuntimeException {
+}
