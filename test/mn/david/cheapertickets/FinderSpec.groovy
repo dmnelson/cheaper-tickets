0e903a4a823de44dc24615ff7de3e52bf7b45362
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-09-13 22:22:44 -0300

    Improving detail on FinderSpec
    Adding more doc on finder spec, explaining what is being done.

--- test/mn/david/cheapertickets/FinderSpec.groovy
+++ test/mn/david/cheapertickets/FinderSpec.groovy
@@ -2,8 +2,8 @@ package mn.david.cheapertickets
 
 import mn.david.cheapertickets.domain.City
 import mn.david.cheapertickets.search.SearchQuery
-import spock.lang.Specification
 import mn.david.cheapertickets.search.engine.Engine
+import spock.lang.Specification
 
 /**
  * User: David Nelson <http://github.com/dmnelson>
@@ -12,18 +12,18 @@ import mn.david.cheapertickets.search.engine.Engine
  */
 class FinderSpec extends Specification {
 
-    def "Searching for tickets"() {
+    def "searching for tickets"() {
 
-        given:
+        given: "a finder that searchs upon a mock engine"
             Engine engine = Mock(Engine);
             Finder finder = new Finder(engine);
 
-        when:
+        when: "a search is made"
             finder.airfares {
                 from 'Belo Horizonte' to 'Porto Alegre' at '12/06/2011'
             }
 
-        then:
+        then: "the engine search method should be called onde"
             1 * engine.search(new SearchQuery(origin: City.BHZ, destination: City.POA, departureDate: Date.parse('dd/MM/yyyy','12/06/2011')));
     }
 }
